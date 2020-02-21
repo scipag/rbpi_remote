328cdb80bc36ae9078da0887d5e58cf07d77a96f
Ahmet Hrnjadovic <ahhr@scip.ch>
2020-02-21 13:45:10 +0100

    Add overwrite option and short arg handling

--- prep.sh
+++ prep.sh
@@ -17,17 +17,42 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+helptext="Usage: prep.sh <options>
+
+Options:
+ -o, --overwrite    overwrite configuration files even if they exist (default:
+                    do not overwrite)
+ -c, --config       config file to use (default: \"config.sh\" in the directory
+                    the script resides in)
+ -h, --help         print this help text and exit"
+
+# set default config file
+_config="$(dirname "$(readlink -f "$0")")/config.sh"
+
+overwrite=0
+for ((i=1; i<=$#; i++)); do
+    case ${!i} in
+        -o|--overwrite) overwrite=1;;
+        -c|--config)    ((i++)); _config="${!i}";;
+        -h|--help)      echo "$helptext"; exit 0;;
+        *) echo "Unknown argument: ${!i@Q}" >&2; exit 1;;
+    esac
+done
+
 ((UID == 0 || EUID == 0)) || { echo "Script needs root permissions to install packages and write config files" >&2; exit 1; }
 
 apt install udhcpd hostapd hping3 ncat iptables usb-modeswitch
 
-_scriptdir=$(dirname "$(readlink -f "$0")")
-
-source "${_scriptdir}/config.sh"
+if [[ -r "$_config" ]]; then
+    source "$_config"
+else
+    echo "Config file ${_config@Q} does not exist or is not readable" >&2
+    exit 1
+fi
 
 [[ -d "$UDHCPD_DIR" ]] || mkdir -pm 755 "$UDHCPD_DIR"
 
-if [[ -f "$UDHCPD_DIR/$UDHCPD_FILE" && "$PRESERVE_CONFIGURATION" == 1 ]]
+if [[ -f "$UDHCPD_DIR/$UDHCPD_FILE" && "$overwrite" -eq 0 ]]
 then
     echo "$UDHCPD_DIR/$UDHCPD_FILE exists; will not overwrite"
 else
@@ -36,7 +61,7 @@ fi
 
 [[ -d "$HOSTAPD_DIR" ]] || mkdir -pm 755 "$HOSTAPD_DIR"
 
-if [[ -f "$HOSTAPD_DIR/$HOSTAPD_FILE" && "$PRESERVE_CONFIGURATION" == 1 ]]
+if [[ -f "$HOSTAPD_DIR/$HOSTAPD_FILE" && "$overwrite" -eq 0 ]]
 then
     echo "$HOSTAPD_DIR/$HOSTAPD_FILE exists, will not overwrite"
 else
@@ -45,12 +70,20 @@ fi
 
 [[ -d "$UNIT_FILE_DIR" ]] || { echo "Systemd directory not found: $UNIT_FILE_DIR" >&2; exit 1; }
 
-if [[ -f "$UNIT_FILE_DIR/$UNIT_FILE_NAME" && "$PRESERVE_CONFIGURATION" == 1 ]]
+if [[ -f "$UNIT_FILE_DIR/$UNIT_FILE_NAME" && "$overwrite" -eq 0 ]]
 then
     echo "$UNIT_FILE_DIR/$UNIT_FILE_NAME exists, will not overwrite"
     echo "Make sure the path to the phone_home script in the unit file is correct"
 else
     printf "%s" "$UNIT_FILE_CONF" >"$UNIT_FILE_DIR/$UNIT_FILE_NAME"
+fi
+
+# rudimentary check for ClientAliveInterval in the default sshd_config
+if [[ -z "$(grep -oiE "^ClientAliveInterval" /etc/ssh/sshd_config)" ]]; then
+    echo "Warning:"
+    echo "Make sure to use a mechanism like SSHs \"ClientAliveInterval\" to avoid connection dropping"
+    echo "and make sure the intervals are lower than what is set with ncats \"--idle-timeout\" option"
+    echo "in the phone_home.sh script"
 fi
 
 systemctl enable "$UNIT_FILE_NAME"
